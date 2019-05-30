909bc1c3769131dfa7a70b8ec751cada1563e37e
xiangxingx <xiangxingx@gmail.com>
2019-05-30 18:04:54 +0800

    新无缝轮播

--- newslide/main.js
+++ newslide/main.js
@@ -1,43 +1,148 @@
+let $buttons = $('#btn > svg')
+console.log($buttons)
+let $slides = $('#slides')
+let $images = $slides.children('img')
+let current = 0
 
-let n
-initialize()
+makeFakeSlides()
 
-setInterval(() => {
-  makeLeave(getImage(n))
-    .one('transitionend', (e) => {
-      makeEnter($(e.currentTarget))
-    })
-  makeCurrent(getImage(n + 1))
-  n += 1
-}, 1500);
+$slides.hide().offset()
+$slides.css({ transform: 'translateX(-400px)' }).show()
+$buttons.eq(0).addClass('active')
 
-function getImage(n) {
-  return $(`.images > img:nth-child(${x(n)})`)
-}
+bindEvents()
 
-function makeCurrent($node) {
-  return $node.removeClass('enter leave').addClass('current')
-}
-function makeLeave($node) {
-  return $node.removeClass('current enter').addClass('leave')
-}
-function makeEnter($node) {
-  return $node.removeClass('leave current').addClass('enter')
-}
+$(next).on('click', function () {
+  goToSlide(current + 1)
+})
+$(previous).on('click', function () {
+  goToSlide(current - 1)
+})
 
-// 初始化状态
-function initialize() {
-  n = 1
-  $(`.images > img:nth-child(${n})`).addClass('current')
-    .siblings().addClass('enter')
-}
+let timer = setInterval(() => {
+  goToSlide(current + 1)
+}, 2000);
 
-function x(n) {
-  if (n > 6) {
-    n = n % 6
-    if (n === 0) {
-      n = 6
-    }
+$('.container').on('mouseenter', function () {
+  window.clearInterval(timer)
+})
+$('.container').on('mouseleave', function () {
+  timer = setInterval(() => {
+    goToSlide(current + 1)
+  }, 2000);
+})
+
+document.addEventListener('visibilitychange', function () {
+  if (document.hidden) {
+    window.clearInterval(timer)
+  }else{
+    timer = setInterval(() => {
+      goToSlide(current + 1)
+    }, 2000);
   }
-  return n
+})
+
+// 整理前的代码
+/* $buttons.eq(0).on('click', function (ev) {
+  if (current === 5) {
+    console.log('最后一张来的')
+    $slides.css({ transform: 'translateX(-2800px)' })
+      .one('transitionend', function () {
+        $slides.hide().offset()//解决切换时过渡动画的问题
+        $slides.css({ transform: 'translateX(-400px)' }).show()
+      })
+  } else {
+    $slides.css({ transform: 'translateX(-400px)' })
+  }
+  $buttons.removeClass('active')
+  $(ev.currentTarget).addClass('active')
+  current = 0
+})
+
+$buttons.eq(1).on('click', function (ev) {
+  $slides.css({ transform: 'translateX(-800px)' })
+  $buttons.removeClass('active')
+  $(ev.currentTarget).addClass('active')
+  current = 1
+})
+
+$buttons.eq(2).on('click', function (ev) {
+  $slides.css({ transform: 'translateX(-1200px)' })
+  $buttons.removeClass('active')
+  $(ev.currentTarget).addClass('active')
+  current = 2
+})
+
+$buttons.eq(3).on('click', function (ev) {
+  $slides.css({ transform: 'translateX(-1600px)' })
+  $buttons.removeClass('active')
+  $(ev.currentTarget).addClass('active')
+  current = 3
+})
+
+$buttons.eq(4).on('click', function (ev) {
+  $slides.css({ transform: 'translateX(-2000px)' })
+  $buttons.removeClass('active')
+  $(ev.currentTarget).addClass('active')
+  current = 4
+})
+
+$buttons.eq(5).on('click', function (ev) {
+  if (current === 0) {
+    console.log('第一张来的')
+    $slides.css({ transform: 'translateX(0px)' })
+      .one('transitionend', function () {
+        $slides.hide().offset()//解决切换时过渡动画的问题
+        $slides.css({ transform: 'translateX(-2400px)' }).show()
+      })
+  } else {
+    $slides.css({ transform: 'translateX(-2400px)' })
+  }
+  $buttons.removeClass('active')
+  $(ev.currentTarget).addClass('active')
+  current = 5
+}) */
+
+
+function bindEvents() {
+  $('#btn').on('click', 'svg', function (ev) {
+    let $button = $(ev.currentTarget)
+    let index = $button.index() // 
+    goToSlide(index)
+
+    $buttons.removeClass('active')
+    $button.addClass('active')
+  })
+}
+
+function goToSlide(index) {
+  if (index > $buttons.length - 1) {
+    index = 0
+  } else if (index < 0) {
+    index = $buttons.length - 1
+  }
+  if (current === $buttons.length - 1 && index === 0) {
+    $slides.css({ transform: `translateX(${-($buttons.length + 1) * 400}px)` })
+      .one('transitionend', function () {
+        $slides.hide().offset()
+        $slides.css({ transform: `translateX(${-(index + 1) * 400}px)` }).show()
+      })
+  } else if (current === 0 && index === $buttons.length - 1) {
+
+    $slides.css({ transform: `translateX(0px)` })
+      .one('transitionend', function () {
+        $slides.hide().offset()
+        $slides.css({ transform: `translateX(${-(index + 1) * 400}px)` }).show()
+      })
+  } else {
+    $slides.css({ transform: `translateX(${-(index + 1) * 400}px)` })
+  }
+  current = index
+}
+
+function makeFakeSlides() {
+  let $firstCopy = $images.eq(0).clone(true) // 同时克隆子元素
+  let $lastCopy = $images.eq($images.length - 1).clone(true)
+  $slides.append($firstCopy)
+  $slides.prepend($lastCopy) // 在前面加上
 }
